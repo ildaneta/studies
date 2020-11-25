a817cf264f47e3cba55a6ee8e74ff742a095505d
Ilda Neta <iildaemanoely@gmail.com>
2020-11-25 14:48:14 -0300

    feat: creating a reducer

---
+++ storeredux/src/store/modules/cart/reducer.ts
@@ -0,0 +1,12 @@
+import { Reducer } from "react";
+import { ICartState } from "./types";
+
+const INITIAL_STATE: ICartState = {
+  items: []
+}
+
+const cart: Reducer<ICartState, ''> = () => {
+  return INITIAL_STATE
+}
+
+export default cart;
