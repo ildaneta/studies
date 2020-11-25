740b2818e2a5cefe7eb48377d9dd4d42ab76f12f
Ilda Neta <iildaemanoely@gmail.com>
2020-11-25 14:47:38 -0300

    feat: creating a catalog component

---
+++ storeredux/src/components/Catalog.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux'
+
+const Catalog = () => {
+  const catalog = useSelector(state => state);
+
+  console.log(catalog)
+  return (
+    <h1>Catalog</h1>
+  );
+}
+
+export default Catalog
