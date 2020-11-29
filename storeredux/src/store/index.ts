e7009f3a488ca3d83fc0ddd70c4ca728548efcb7
Ilda Neta <iildaemanoely@gmail.com>
2020-11-29 14:37:00 -0300

    chore: adding the redux devtools library and using in sotre file

--- storeredux/src/store/index.ts
+++ storeredux/src/store/index.ts
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
+import { ICartState } from './modules/cart/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+export interface IState {
+  cart: ICartState,
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 export default store
