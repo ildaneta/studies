1554a13b8df2ce03a6cacd3e951692140f3594c4
Ilda Neta <iildaemanoely@gmail.com>
2020-11-29 12:34:06 -0300

    feat: changing the redux import

--- storeredux/src/store/modules/cart/reducer.ts
+++ storeredux/src/store/modules/cart/reducer.ts
@@ -1,11 +1,11 @@
-import { Reducer } from "react";
+import { Reducer } from "redux";
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
   items: []
 }
 
-const cart: Reducer<ICartState, ''> = () => {
+const cart: Reducer<ICartState> = () => {
   return INITIAL_STATE
 }
 
