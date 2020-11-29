6506dbf088c12bf976d9e2670daabf6a9fd6b4e2
Ilda Neta <iildaemanoely@gmail.com>
2020-11-29 14:36:11 -0300

    feat: incrementing the cart reducer file

--- storeredux/src/store/modules/cart/reducer.ts
+++ storeredux/src/store/modules/cart/reducer.ts
@@ -1,12 +1,39 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
   items: []
 }
 
-const cart: Reducer<ICartState> = () => {
-  return INITIAL_STATE
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  return produce(state, draft => {
+
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+
+        const productInCartIndex = draft.items.findIndex(
+          item => item.product.id === product.id,
+        )
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1
+          })
+        }
+
+
+        break;
+      }
+      default: {
+        return state
+      }
+    }
+  })
 }
 
 export default cart;
