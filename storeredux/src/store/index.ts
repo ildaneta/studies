34228e0c194fd36efbd1bda6934099fb4e471233
Ilda Neta <iildaemanoely@gmail.com>
2020-11-30 07:10:10 -0300

    feat: adding the middleware saga

--- storeredux/src/store/index.ts
+++ storeredux/src/store/index.ts
@@ -1,16 +1,26 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { ICartState } from './modules/cart/types';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
   cart: ICartState,
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
 const store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store
