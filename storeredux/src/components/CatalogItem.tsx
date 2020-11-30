cd34da280b201c50da8c69783982e73368dba59e
Ilda Neta <iildaemanoely@gmail.com>
2020-11-30 07:09:49 -0300

    feat: checking the cart stock and show the failed stock message

--- storeredux/src/components/CatalogItem.tsx
+++ storeredux/src/components/CatalogItem.tsx
@@ -1,7 +1,8 @@
 import { useCallback } from 'react';
-import { addProducToCart } from '../store/modules/cart/actions';
-import { useDispatch } from 'react-redux';
+import { addProducToCartRequest } from '../store/modules/cart/actions';
+import { useDispatch, useSelector } from 'react-redux';
 import { IProduct } from "../store/modules/cart/types";
+import { IState } from '../store';
 
 interface ICatalogItemProps {
   product: IProduct;
@@ -10,8 +11,12 @@ interface ICatalogItemProps {
 const CatalogItem = ({ product }: ICatalogItemProps) => {
   const dispatch = useDispatch()
 
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id)
+  })
+
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProducToCart(product))
+    dispatch(addProducToCartRequest(product))
   }, [dispatch, product])
 
   return (
@@ -21,10 +26,12 @@ const CatalogItem = ({ product }: ICatalogItemProps) => {
 
       <button
         type="button"
-        onClick={() => handleAddProductToCart}
+        onClick={handleAddProductToCart}
       >
         Comprar
       </button>
+
+      {hasFailedStockCheck && <span style={{ color: 'red' }}>Falta de estoque</span>}
     </article>
   )
 }
