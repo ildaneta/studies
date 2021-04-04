70da3433176cba3a85479f3327b1649b365ce0dd
Ilda Neta <iildaemanoely@gmail.com>
2021-04-04 18:37:32 -0300

    chore: Starting a new react native project

---
+++ React-case-studie-festival-2021/reactJS02/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>React JS Case Study Festival 2021</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
