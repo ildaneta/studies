f6f9d630fefb379b52456b8953a77878e55d2d2b
Ilda Neta <iildaemanoely@gmail.com>
2021-04-04 18:38:35 -0300

    creating anew project

--- React-case-studie-festival-2021/reactJS/ios/Pods/Target Support Files/Pods-reactJS-reactJSTests/Pods-reactJS-reactJSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reactJS_reactJSTests : NSObject
-@end
-@implementation PodsDummy_Pods_reactJS_reactJSTests
-@end
--- React-case-studie-festival-2021/reactJS/ios/Pods/Target Support Files/Pods-reactJS/Pods-reactJS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reactJS : NSObject
-@end
-@implementation PodsDummy_Pods_reactJS
-@end
