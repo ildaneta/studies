f5553daabeb4eca86cbda309f1ac6fa712e45785
Ilda Neta <iildaemanoely@gmail.com>
2021-04-04 18:02:58 -0300

    chore: Creating a new react native project

---
+++ React-case-studie-festival-2021/reactJS/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+
+      <Text>Hello React JS Case Study Festival 2021</Text>
+
+      <View>
+        <Text></Text> 
+        <Text></Text>
+      </View>
+
+      <Text>Hello React JS Case Study Festival 2021 Hello React JS Case Study Festival 2021Hello React JS Case Study Festival 2021Hello React JS Case Study Festival 2021Hello React JS Case Study Festival 2021Hello React JS Case Study Festival 2021</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
